bb32e0ee8129aaa02aba50106b441eeacabfcb40
mvdevelop <marcosvmdilly@gmail.com>
2025-12-15 16:00:19 -0300

    feat: adding database

--- src/main/java/com/exemplo/api/ApiApplication.java
+++ src/main/java/com/exemplo/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.exemplo.api;
+package com.exemplo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
